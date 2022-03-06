2d455fb30e2059dc2ae446d90b8189c0fab294a5
08msawyer <08msawyer@brightoncollege.net>
2022-03-06 17:22:40 +0000

    Statue: Now requires 200XP, shows messages to the user and makes running animation faster.

--- Assets/Scripts/StatueSpeedBuff.cs
+++ Assets/Scripts/StatueSpeedBuff.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 
 public class StatueSpeedBuff : NetworkBehaviour
 {
+    private static readonly int RunSpeedMultiplier = Animator.StringToHash("RunSpeedMultiplier");
+    
     private bool _activated;
+
+    public int xpThreshold = 200;
+    public float speedMultiplier = 1.5f;
 
     // If the statue collides with something it will be given a speed boost
     private void OnTriggerEnter(Collider other)
@@ -11,21 +16,35 @@ public class StatueSpeedBuff : NetworkBehaviour
         if (!IsServer) return;
 
         var animal = other.gameObject.GetComponent<AnimalMovementController>();
+        var hud = other.gameObject.GetComponent<PlayerHudManager>();
         if (animal != null)
         {
-            if (_activated) return;
+            if (_activated)
+            {
+                hud.ShowMessage($"The statue has already been used!", 3f);
+                return;
+            }
+
+            if (hud.Xp < xpThreshold)
+            {
+                hud.ShowMessage($"You need {xpThreshold}XP to use the statue!", 3f);
+                return;
+            }
+
             _activated = true;
             MakeFoxSpeedyClientRpc(new ClientRpcSendParams
             {
                 TargetClientIds = new[] {animal.OwnerClientId}
             });
-            other.gameObject.GetComponent<PlayerHudManager>().ShowScreen("FoxStatueScreen", 5f);
+            hud.ShowScreen("FoxStatueScreen", 5f);
         }
     }
 
     [ClientRpc]
     private void MakeFoxSpeedyClientRpc(ClientRpcSendParams parameters)
     {
-        NetworkManager.LocalClient.PlayerObject.GetComponent<AnimalMovementController>().playerSpeed *= 1.5f;
+        var localPlayer = NetworkManager.LocalClient.PlayerObject;
+        localPlayer.GetComponent<AnimalMovementController>().playerSpeed *= speedMultiplier;
+        localPlayer.GetComponent<AnimalAnimationController>().SetFloat(RunSpeedMultiplier, speedMultiplier);
     }
 }
