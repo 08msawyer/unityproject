d8c6ad5084b7df3f5de62cfbc36c5b80d722723a
08msawyer <08msawyer@brightoncollege.net>
2022-03-06 17:21:41 +0000

    HUD: Add XP and message system.

--- Assets/Scripts/PlayerHudManager.cs
+++ Assets/Scripts/PlayerHudManager.cs
@@ -1,12 +1,55 @@
+using System;
 using System.Collections;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerHudManager : NetworkBehaviour
 {
+    private TextMeshProUGUI _xpText;
+    private TextMeshProUGUI _messageText;
+    private NetworkVariable<int> _xp = new();
+
+    internal int Xp => _xp.Value;
+
+    private void Start()
+    {
+        var xpObject = GameObject.FindWithTag("HUD").transform.Find("Canvas").Find("XP").gameObject;
+        _xpText = xpObject.GetComponent<TextMeshProUGUI>();
+        var messageObject = GameObject.FindWithTag("HUD").transform.Find("Canvas").Find("Message").gameObject;
+        _messageText = messageObject.GetComponent<TextMeshProUGUI>();
+    }
+
     public override void OnNetworkSpawn()
     {
         ShowScreen("FoxStartScreen", 5f);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsClient) return;
+        _xpText.text = $"XP: {Xp}";
+    }
+
+    public void AddXp(int amount)
+    {
+        _xp.Value += amount;
+    }
+
+    public void ShowMessage(string message, float duration)
+    {
+        ShowMessageClientRpc(message, duration, new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams {
+                TargetClientIds = new[] {OwnerClientId}
+            }
+        });
+    }
+
+    [ClientRpc]
+    private void ShowMessageClientRpc(string message, float duration, ClientRpcParams clientRpcParams = default)
+    {
+        StartCoroutine(ShowMessageCoroutine(message, duration));
     }
 
     public void ShowScreen(string screenName, float duration)
@@ -23,6 +66,13 @@ public class PlayerHudManager : NetworkBehaviour
     private void ShowScreenClientRpc(string screenName, float duration, ClientRpcParams clientRpcParams = default)
     {
         StartCoroutine(ShowScreenCoroutine(screenName, duration));
+    }
+
+    private IEnumerator ShowMessageCoroutine(string message, float duration)
+    {
+        _messageText.text = message;
+        yield return new WaitForSeconds(duration);
+        _messageText.text = string.Empty;
     }
 
     private IEnumerator ShowScreenCoroutine(string screenName, float duration)
